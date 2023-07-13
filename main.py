afeff48a76dbc1c5911ba76c9461b1c084bf979b
Space-Penguins <hampus.berg@protonmail.com>
2023-07-13 10:55:28 +0200

    Re-establish the database connection when handling a request

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 import os
 from flask import Flask, render_template, request, url_for
 import mysql.connector
-
-
-# Retrieve the MySQL database credentials from environment variables
-mysql_host = os.environ.get('MYSQLHOST')
-mysql_user = os.environ.get('MYSQLUSER')
-mysql_password = os.environ.get('MYSQLPASSWORD')
-mysql_database = os.environ.get('MYSQLDATABASE')
-mysql_port = int(os.environ.get('MYSQLPORT', 3306))
-
-# Connect to the MySQL database
-connection = mysql.connector.connect(
-    host=mysql_host,
-    user=mysql_user,
-    password=mysql_password,
-    database=mysql_database,
-    port=mysql_port
-)
-
-# Create a cursor object to execute SQL queries
-cursor = connection.cursor()
-
+import logging
 
 app = Flask(__name__)
 
+# Configure logging
+app.logger.setLevel(logging.WARNING)
+stream_handler = logging.StreamHandler()
+app.logger.addHandler(stream_handler)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     if request.method == "POST":
-
         participant = request.form.get("participant")
 
         query = """
@@ -40,15 +22,25 @@ def index():
                 JOIN Events ON Participants_Events.event_id = Events.event_id
                 WHERE Participants.participant_name = %s;
                 """
-        cursor.execute(query, (participant,))
-        events = cursor.fetchall()
+        # Establish a new database connection
+        with mysql.connector.connect(
+            host=os.environ.get('MYSQLHOST'),
+            user=os.environ.get('MYSQLUSER'),
+            password=os.environ.get('MYSQLPASSWORD'),
+            database=os.environ.get('MYSQLDATABASE'),
+            port=int(os.environ.get('MYSQLPORT', 3306))
+        ) as connection:
+            cursor = connection.cursor()
+            
+            cursor.execute(query, (participant,))
+            events = cursor.fetchall()
 
-        eventList = []
+            eventList = []
 
-        for event in events:
-            eventList.append(event[0])
+            for event in events:
+                eventList.append(event[0])
 
-        return render_template("events.html", eventList=eventList, name=participant)
+            return render_template("events.html", eventList=eventList, name=participant)
 
     return render_template("index.html")
 
